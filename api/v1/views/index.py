8d47033bbd889e4aa02daa0b41ddb13221782fdb
oumatoulacen <oumatoulacen@gmail.com>
2023-10-29 21:19:11 +0100

    add 200 on response

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 @app_views.route("/status")
 def status():
     """api status"""
-    return {"status": "OK"}
+    return {"status": "OK"}, 200
 
 
 @app_views.route("/stats")
