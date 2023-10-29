b8eb8a17b3f81417bd20c6cdbba89067f5018335
oumatoulacen <oumatoulacen@gmail.com>
2023-10-29 03:22:05 +0100

    add api module

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,8 +16,9 @@ def close_storage(exception):
     '''close the storage'''
     storage.close()
 
+host = environ.get('HBNB_API_HOST', '0.0.0.0')
+port = environ.get('HBNB_API_PORT', 5000)
 
 if __name__ == "__main__":
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = environ.get('HBNB_API_PORT', 5000)
+    """api entrypoint"""
     app.run(debug=True, port=port, host=host, threaded=True)
