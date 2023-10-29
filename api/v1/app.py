bfb7f52b128b561ab0139c1b9b4ed4677d62be57
oumatoulacen <oumatoulacen@gmail.com>
2023-10-29 18:24:28 +0100

    change vars

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,17 +13,38 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 
 @app.errorhandler(404)
 def page_not_found(e):
+    """_summary_
+
+    Args:
+        e (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return {"error": "Not found"}, 404
 
 
 @app.errorhandler(400)
 def page_not_found(e):
+    """_summary_
+
+    Args:
+        e (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     message = e.description
     return message, 400
 
 
 @app.teardown_appcontext
 def close(ctx):
+    """_summary_
+
+    Args:
+        ctx (_type_): _description_
+    """
     storage.close()
 
 
@@ -39,4 +60,5 @@ else:
 
 
 if __name__ == "__main__":
+    """" start app"""
     app.run(host=host, port=port, threaded=True)
