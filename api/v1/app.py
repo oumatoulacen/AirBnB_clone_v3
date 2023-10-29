03f1017aba70d48abb3644c67a433cd875e330f9
oumatoulacen <oumatoulacen@gmail.com>
2023-10-29 18:30:28 +0100

    docs

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python3
-"""Endpoint (route) will be to return the status of your API"""
-import os
-from flask import Flask
-from models import storage
+""" AirBnB v3 flask Api v1 entrypoint """
+from flask import Flask, make_response
+from flask_cors import CORS
+import json
 from api.v1.views import app_views
+from os import getenv
 
-# creating a Flask app
 app = Flask(__name__)
-
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+host = getenv("HBNB_API_HOST")
+port = getenv("HBNB_API_PORT")
+cors = CORS(
+    app,
+    resources={r"/*": {"origins": "0.0.0.0"}}
+    )
 
 
 @app.teardown_appcontext
-def close(ctx):
-    """ close session
-    """
+def teardown(err):
+    """api teardown"""
+    from models import storage
+
     storage.close()
 
 
-if os.getenv("HBNB_API_HOST"):
-    host = os.getenv("HBNB_API_HOST")
-else:
-    host = "0.0.0.0"
-
-if os.getenv("HBNB_API_PORT"):
-    port = int(os.getenv("HBNB_API_PORT"))
-else:
-    port = 5000
+@app.errorhandler(404)
+def not_found(err):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    res = {'error': "Not found"}
+    response = make_response(json.dumps(res), 404)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 
 if __name__ == "__main__":
-    """" start app"""
+    """api entrypoint"""
+    host = "0.0.0.0" if host is None else host
+    port = "5000" if port is None else port
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-''' create routs for views'''
-from api.v1.views import app_views
-from flask import jsonify
-
-
+"""api index"""
 from api.v1.views import app_views
 
 
 @app_views.route("/status")
 def status():
     """api status"""
-    return jsonify({"status": "OK"})
+    return {"status": "OK"}
