77eb8d530e55d9b07df9537e35e34913590f1adc
oumatoulacen <oumatoulacen@gmail.com>
2023-10-29 01:26:47 +0100

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,7 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 
-host = environ.get('HBNB_API_HOST', '0.0.0.0')
-port = environ.get('HBNB_API_PORT', '5000')
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -18,4 +17,6 @@ def close_storage(exception):
 
 
 if __name__ == "__main__":
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = environ.get('HBNB_API_PORT', '5000')
     app.run(debug=True, port=port, host=host, threaded=True)
