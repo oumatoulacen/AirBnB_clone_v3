7e0e09589f57e03907fc765749beb5fe38514b71
oumatoulacen <oumatoulacen@gmail.com>
2023-10-29 18:13:06 +0100

    change vars

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,46 @@
 #!/usr/bin/python3
-'''start API application'''
-
-from flask import Flask
+""" AirBnB v3 flask Api v1 entrypoint """
+from flask import Flask, make_response
 from flask_cors import CORS
-from models import storage
+import json
 from api.v1.views import app_views
-from os import environ
-
+from os import getenv
 
 app = Flask(__name__)
-CORS(app)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+host = getenv("HBNB_API_HOST")
+port = getenv("HBNB_API_PORT")
+cors = CORS(
+    app,
+    resources={r"/*": {"origins": "0.0.0.0"}}
+    )
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    '''close the storage'''
+def teardown(err):
+    """api teardown"""
+    from models import storage
+
     storage.close()
 
 
-host = environ.get('HBNB_API_HOST')
-port = environ.get('HBNB_API_PORT')
+@app.errorhandler(404)
+def not_found(err):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    res = {'error': "Not found"}
+    response = make_response(json.dumps(res), 404)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 
 if __name__ == "__main__":
     """api entrypoint"""
-    port = port if port is not None else 5000
-    host = host if host is not None else '0.0.0.0'
-    app.run(port=port, host=host, threaded=True)
+    host = "0.0.0.0" if host is None else host
+    port = "5000" if port is None else port
+    app.run(host=host, port=port, threaded=True)
