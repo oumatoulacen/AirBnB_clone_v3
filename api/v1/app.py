e237e7df9ecc9cbe6f03acc7862555d4d1810f98
oumatoulacen <oumatoulacen@gmail.com>
2023-10-29 18:16:04 +0100

    change vars

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,32 +11,28 @@ app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 host = getenv("HBNB_API_HOST")
 port = getenv("HBNB_API_PORT")
-cors = CORS(
-    app,
-    resources={r"/*": {"origins": "0.0.0.0"}}
-    )
-
+CORS(app)
+#resources={r"/*": {"origins": "0.0.0.0"}}
 
 @app.teardown_appcontext
 def teardown(err):
     """api teardown"""
     from models import storage
-
     storage.close()
 
 
-@app.errorhandler(404)
-def not_found(err):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
-    res = {'error': "Not found"}
-    response = make_response(json.dumps(res), 404)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+# @app.errorhandler(404)
+# def not_found(err):
+#     """ 404 Error
+#     ---
+#     responses:
+#       404:
+#         description: a resource was not found
+#     """
+#     res = {'error': "Not found"}
+#     response = make_response(json.dumps(res), 404)
+#     response.headers['Content-Type'] = 'application/json'
+#     return response
 
 
 if __name__ == "__main__":
