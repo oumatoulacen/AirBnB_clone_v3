165261c4746e14da380cf3bd8819f468bddbc3bf
oumatoulacen <oumatoulacen@gmail.com>
2023-10-29 01:10:42 +0100

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,18 +6,18 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 
-host = environ.get('HBNB_API_HOST', '0.0.0.0')
-port = environ.get('HBNB_API_PORT', '5000')
-app = Flask(__name__)
-
-app.register_blueprint(app_views)
-
-
-@app.teardown_appcontext
-def close_storage(exception):
-    '''close the storage'''
-    storage.close()
-
-
 if __name__ == "__main__":
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = environ.get('HBNB_API_PORT', '5000')
+    app = Flask(__name__)
+
+    app.register_blueprint(app_views)
+
+
+    @app.teardown_appcontext
+    def close_storage(exception):
+        '''close the storage'''
+        storage.close()
+
+
     app.run(debug=True, port=port, host=host, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 ''' create views blueprints'''
 from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
-
-# flake8: noqa
-
-app_views = Blueprint('bp_views', __name__, url_prefix='/api/v1')
