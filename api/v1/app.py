92e94061b7d1ed23806c8e06982a934688336bd4
oumatoulacen <oumatoulacen@gmail.com>
2023-10-29 18:27:00 +0100

    docs

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,39 +11,9 @@ app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """_summary_
-
-    Args:
-        e (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    return {"error": "Not found"}, 404
-
-
-@app.errorhandler(400)
-def page_not_found(e):
-    """_summary_
-
-    Args:
-        e (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    message = e.description
-    return message, 400
-
-
 @app.teardown_appcontext
 def close(ctx):
-    """_summary_
-
-    Args:
-        ctx (_type_): _description_
+    """ close session
     """
     storage.close()
 
