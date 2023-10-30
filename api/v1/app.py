d41fe745011f1282a0bd66356918f618ba3f0a0e
oumatoulacen <oumatoulacen@gmail.com>
2023-10-31 00:48:53 +0100

    update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,46 +1,27 @@
 #!/usr/bin/python3
-""" AirBnB v3 flask Api v1 entrypoint """
-from flask import Flask, make_response
-from flask_cors import CORS
-import json
+"""app.py to connect to API"""
+import os
+from models import storage
 from api.v1.views import app_views
-from os import getenv
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-host = getenv("HBNB_API_HOST")
-port = getenv("HBNB_API_PORT")
-cors = CORS(
-    app,
-    resources={r"/*": {"origins": "0.0.0.0"}}
-    )
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(err):
-    """api teardown"""
-    from models import storage
-
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(err):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
-    res = {'error': "Not found"}
-    response = make_response(json.dumps(res), 404)
-    response.headers['Content-Type'] = 'application/json'
-    return response
-
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    """api entrypoint"""
-    host = "0.0.0.0" if host is None else host
-    port = "5000" if port is None else port
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
