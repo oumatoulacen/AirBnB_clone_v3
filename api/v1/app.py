03cf78caa7e211afb25eedfd43fe2e2ea812ab7d
oumatoulacen <oumatoulacen@gmail.com>
2023-11-02 16:26:51 +0100

    cascade

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
